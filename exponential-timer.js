c39b06c588991967fe45f2b4fb03dad9619f68a6
iamandymcinnes <andy.mcinnes@kalibrate.com>
2021-01-03 01:30:27 +0000

    fixed error undefined

--- exponential-timer.js
+++ exponential-timer.js
@@ -5,39 +5,39 @@ module.exports = function(RED) {
         var context = node.context();
 
         node.on('input', function(msg, send, done) {
-            let timeout = context.get("timer");
-            let timeoutId = context.get("timeoutId");
-            clearTimeout(timeoutId);
-            
-            node.scalefactor = config.scalefactor;
-            node.duration = config.duration;
-            node.maxduration = config.maxduration;
-            
-            if(msg.reset) {
-                context.set("timer",null);
-                node.status({text: 'reset'});
-                return;
+            try {
+                let timeout = context.get("timer");
+                let timeoutId = context.get("timeoutId");
+                clearTimeout(timeoutId);
+                
+                node.scalefactor = config.scalefactor;
+                node.duration = config.duration;
+                node.maxduration = config.maxduration;
+                
+                if(msg.reset) {
+                    context.set("timer",null);
+                    node.status({text: 'reset'});
+                    return;
+                }
+                
+                if(timeout) {
+                    timeout = Math.min(node.duration * node.scalefactor, node.maxduration);
+                } else {
+                    timeout = node.duration;
+                }
+                
+                timeoutId = setTimeout(() => {
+                    context.set("timer",null);
+                    node.status({});
+                    node.send(msg);
+                    node.done();
+                }, timeout * 1000);
+                
+                context.set("timer", timeout);
+                context.set("timeoutId", timeoutId);
+                node.status({text: timeout});
             }
-            
-            if(timeout) {
-                timeout = Math.min(node.duration * node.scalefactor, node.maxduration);
-            } else {
-                timeout = node.duration;
-            }
-            
-            timeoutId = setTimeout(() => {
-                context.set("timer",null);
-                node.status({});
-                node.send(msg);
-                node.done();
-            }, timeout * 1000);
-            
-            context.set("timer", timeout);
-            context.set("timeoutId", timeoutId);
-            node.status({text: timeout});
-
-            // If an error is hit, report it to the runtime
-            if (err) {
+            catch(err) {
                 if (done) {
                     // Node-RED 1.0 compatible
                     done(err);
